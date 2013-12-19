119fa3b4ee5314ccea6ba62b4c2c052662f651ec
steve <stevedanko1@gmail.com>
2013-12-19 18:03:20 -0500

    setted up development enviorment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtWord::Application.config.secret_key_base = 'ae5fa57b601126ad1ba3b7b10489c22d4d2caa82ef9aa5390695f1d3f8ff87db0e9c87a73a1114e9e84db35c5070435a9d3e6aa0dbbc918cf63e09f50fbb9e3a'
